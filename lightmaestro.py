4aecde3a9d9094ba3cc89591d444cb233ecb0903
Judson D Neer <judson.neer@gmail.com>
2013-12-09 22:09:25 -0800

    First cut at triggering script.

--- lightmaestro.py
+++ lightmaestro.py
@@ -1,18 +1,25 @@
+#!/usr/bin/env python
+
 import os
 import time
 
-try:
-    from simplecoremidi import send_midi
-except ImportError:
-    def send_midi(command, note, velocity):
-        print 'DEBUG:', command, note, velocity
+import rtmidi
 
-_folder = 'Scenes'
+_folder = 'triggers'
 _polldelay = 0.25
 
 _starttime = time.time()
 
 _atimes = {}
+
+midi = rtmidi.MidiOut()
+usbports = [p for p in midi.get_ports() if 'USB' in p]
+
+if not usbports:
+    print('No USB MIDI adapter found.')
+    exit(1)
+
+midi.open_port(name=usbports[0])
 
 while True:
     time.sleep(_polldelay)
@@ -20,11 +27,11 @@ while True:
         fpath = os.path.join(_folder, f)
         atime = os.stat(fpath).st_atime
         lastatime = _atimes.get(f, _starttime)
+        print(f, lastatime, atime)
         if atime - lastatime > 1.0:
             _atimes[f] = atime
             _, p, _, s = os.path.splitext(f)[0].split()
             i = (int(p) - 1) * 24 + int(s) - 1
             channel, note = divmod(i, 72)
-            send_midi(0x90 | channel, note, 127)
-            time.sleep(_polldelay)
-            send_midi(0x90 | channel, note, 0)  # TODO is the note-off command necessary?
+            midi.send_message((0x90 | channel, note, 127))
+            print('Sent note {0} to channel {1}'.format(note, channel))
