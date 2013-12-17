dc850a0da7fe3bd6147f1080f7a17289afc89cad
Judson D Neer <judson.neer@ngc.com>
2013-12-17 13:19:15 -0800

    Get rid of .json extension on scenes for simplicity.

--- scenes/makescenes.py
+++ scenes/makescenes.py
@@ -3,12 +3,12 @@ import json
 
 
 channels = collections.OrderedDict((str(c+1), 100.0) for c in range(12))
-with open('0.json', 'wb') as f:
+with open('Default', 'wb') as f:
     json.dump({'channels': channels, 'fade': 0.0}, f, indent=4)
 
 
 for i in range(12):
     channels = collections.OrderedDict((str(c+1), 0) for c in range(12))
     channels[str(i+1)] = 100.0
-    with open(str(i+1) + '.json', 'wb') as f:
+    with open(str(i+1), 'wb') as f:
         json.dump({'channels': channels, 'fade': 5.0}, f, indent=4)
