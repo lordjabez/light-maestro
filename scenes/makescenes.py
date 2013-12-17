cc04d6439ba2ff3775d6c5c8005afc942a23fc74
Judson D Neer <judson.neer@ngc.com>
2013-12-17 11:19:59 -0800

    Make scene files use 0.0 thru 100.0 floating point for channel values.

--- scenes/makescenes.py
+++ scenes/makescenes.py
@@ -2,13 +2,13 @@ import collections
 import json
 
 
-channels = collections.OrderedDict((str(c+1), 255) for c in range(12))
+channels = collections.OrderedDict((str(c+1), 100.0) for c in range(12))
 with open('0.json', 'wb') as f:
     json.dump({'channels': channels, 'fade': 0.0}, f, indent=4)
 
 
 for i in range(12):
     channels = collections.OrderedDict((str(c+1), 0) for c in range(12))
-    channels[str(i+1)] = 255
+    channels[str(i+1)] = 100.0
     with open(str(i+1) + '.json', 'wb') as f:
         json.dump({'channels': channels, 'fade': 5.0}, f, indent=4)
