b20ea55302f81d759b3e3f2e0a7e1f5d58bc30b0
Judson D Neer <judson.neer@gmail.com>
2014-02-22 22:50:08 -0800

    New interface for individual light control.

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -5,44 +5,52 @@
 'use strict';
 
 
-// Sends an AJAX command based on scene selection.
-function sendCommand() {
-
-    // Pull the scene identifier from the button label.
-    var sceneId = parseInt($(this).html());
-
-    // Send the request. We don't care about the response for now.
-    $.ajax({method: 'POST', url: '/scenes/' + sceneId + '/_load'});
-
+function selectFixtures() {
+    var type = $(this).html();
+    var whitesChecked = type == 'All' || type == 'Whites';
+    var colorsChecked = type == 'All' || type == 'Colors';
+    $('input[class="fixture-white"]').prop('checked', whitesChecked);
+    $('input[class="fixture-color"]').prop('checked', colorsChecked);
+    $('input[type="checkbox"]').checkboxradio('refresh');
 }
 
+function changeValues(event) {
+    var alpha = parseFloat($('#value-alpha').val());
+    var red = parseFloat($('#value-red').val());
+    var green = parseFloat($('#value-green').val());
+    var blue = parseFloat($('#value-blue').val());
+    var channels = {}
+    $('input[type="checkbox"]').each( function() {
+        if($(this).prop('checked')) {
+           var id = parseInt($(this).attr('id').split('-')[1]);
+           channels[id + 0] = alpha;
+           channels[id + 1] = red;
+           channels[id + 2] = green;
+           channels[id + 3] = blue;
+       }
+    });
+    $.ajax({data: JSON.stringify({'channels': channels})});
+}
 
-// Properly resize buttons when the page size changes.
-$(window).on('resize', function() {
-    $('.ui-btn-inner').css('line-height', '100px');
-    var headerHeight = $('div:jqmData(role="header")').outerHeight();
-    var footerHeight = $('div:jqmData(role="footer")').outerHeight();
-    var targetContentHeight = $(window).outerHeight() - headerHeight - footerHeight;
-    var actualContentHeight = $('div:jqmData(role="content")').outerHeight();
-    var targetLineHeight = 100 + (targetContentHeight - actualContentHeight) / 3;
-    $('.ui-btn-inner').css('line-height', targetLineHeight + 'px');
-});
-
-
-// Executes initialization commands on the page.
+// Things to do before the page is shown.
 $(document).bind('pagebeforeshow', function() {
 
-    // Disable any ability to select text in the GUI
+    // Set up AJAX calls.
+    $.ajaxSetup({method: 'POST', url: '/channels/_load', headers: {'content-type': 'application/json'}});
+
+    // Disable any ability to select text in the GUI.
     $('body').bind('selectstart',function() {
         return false;
     });
 
-    // Bind command send events to scene select buttons
-    $('button.scene-selector').bind('tap', sendCommand);
+    // Bind the select fixture function to selection buttons.
+    $('button').bind('tap', selectFixtures);
+
+    // Bind the change value function to the sliders (TODO make this one call).    
+    $('#value-alpha').bind('change', changeValues);
+    $('#value-red').bind('change', changeValues);
+    $('#value-green').bind('change', changeValues);
+    $('#value-blue').bind('change', changeValues);
 
 });
 
-
-$(document).bind('pageshow', function() {
-    $(window).resize();
-});
